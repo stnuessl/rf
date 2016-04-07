5690be4aac6fc9140c5045d1f43237e68c80ec11
stnuessl <nuessle.steffen@gmail.com>
2016-04-07 20:56:31 +0200

    bash-completion: refactored run.sh: added front up run of the compilation process to avoid         the warning when the main binary does not exist.

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/bash
 
+#
+# Copyright (C) 2016  Steffen Nüssle
+# rf - refactor
+#
+# This file is part of rf.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+g++ -Wall -std=c++11 -o main main.cpp;
 MD5=$(md5sum main | cut -f1 -d " ");
 
 rf --tag n::a=aa,b=bb,c=cc && rf --syntax-only;
