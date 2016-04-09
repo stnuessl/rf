27d8cce941a3158ed689f480425326e25d437fbc
stnuessl <nuessle.steffen@gmail.com>
2016-04-09 13:23:36 +0200

    make_unique: Fixed compile error that occured when automatic template type deduction failed.

--- src/util/memory.hpp
+++ src/util/memory.hpp
@@ -30,7 +30,7 @@ namespace std {
 template <typename T, typename ...Args>
 std::unique_ptr<T> make_unique(Args &&...args)
 {
-    return std::unique_ptr<T>(new T(std::forward(args)...));
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 }
